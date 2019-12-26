692986833a4fcf5b415f2b8837eae7ce6c90dec4
depeng <kongdepeng520@gmail.com>
2019-12-26 13:37:26 +0800

    [feat]: complete

---
+++ commands/init.js
@@ -0,0 +1,106 @@
+// commands/init.js
+const fs = require('fs')
+const shell = require('shelljs');
+const symbols = require('log-symbols');
+const clone = require('../utils/clone.js');
+const remote = 'https://github.com/iamlazyen/vue-ts-template.git';
+let branch = 'vue2';
+
+const initAction = async (name, option) => {
+    // 0. 检查控制台是否可以运行`git `，
+    if (!shell.which('git')) {
+        console.log(symbols.error, '对不起，git命令不可用！');
+        shell.exit(1);
+    }
+    // 1. 验证输入name是否合法
+    if (fs.existsSync(name)) {
+        console.log(symbols.warning,`已存在项目文件夹${name}！`);
+        return;
+    }
+    if (name.match(/[^A-Za-z0-9\u4e00-\u9fa5_-]/g)) {
+        console.log(symbols.error, '项目名称存在非法字符！');
+        return;
+    }
+    // 2. 获取option，确定模板类型（分支）
+    if (option.dev) branch = 'develop';
+
+
+      // init.js
+    const inquirer = require('inquirer');
+    // 定义需要询问的问题
+    const questions = [
+      {
+        type: 'input',
+        message: '请输入模板名称:',
+        name: 'name',
+        validate(val) {
+          if (!val) return '模板名称不能为空！';
+          if (val.match(/[^A-Za-z0-9\u4e00-\u9fa5_-]/g)) return '模板名称包含非法字符，请重新输入';
+          return true;
+        }
+      },
+      {
+        type: 'input',
+        message: '请输入模板关键词（;分割）:',
+        name: 'keywords'
+      },
+      {
+        type: 'input',
+        message: '请输入模板简介:',
+        name: 'description'
+      },
+      {
+        type: 'list',
+        message: '请选择模板类型:',
+        choices: ['响应式', '桌面端', '移动端'],
+        name: 'type'
+      },
+      {
+        type: 'list',
+        message: '请选择模板分类:',
+        choices: ['整站', '单页', '专题'],
+        name: 'category'
+      },
+      {
+        type: 'input',
+        message: '请输入模板风格:',
+        name: 'style'
+      },
+      {
+        type: 'input',
+        message: '请输入模板色系:',
+        name: 'color'
+      },
+      {
+        type: 'input',
+        message: '请输入您的名字:',
+        name: 'author'
+      }
+    ];
+    // 通过inquirer获取到用户输入的内容
+    const answers = await inquirer.prompt(questions);
+    // 将用户的配置打印，确认一下是否正确
+    console.log('------------------------');
+    console.log(answers);
+    let confirm = await inquirer.prompt([
+        {
+            type: 'confirm',
+            message: '确认创建？',
+            default: 'Y',
+            name: 'isConfirm'
+        }
+    ]);
+    if (!confirm.isConfirm) return false;
+
+
+
+
+    // 4. 下载模板
+    await clone(`direct:${remote}#${branch}`, name, { clone: true });
+
+    const deleteDir = ['.git', '.gitignore', 'README.md', 'docs']; // 需要清理的文件
+    const pwd = shell.pwd();
+    deleteDir.map(item => shell.rm('-rf', pwd + `/${name}/${item}`));
+};
+
+module.exports = initAction;
--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
+const commander = require('commander');
+const initAction = require('./commands/init')
 
-console.log('hello cli');
+commander.version(require('./package.json').version).option('-v,--version', '查看版本号');
+
+commander
+    .command('init <name>') // 定义init子命令，<name>为必需参数可在action的function中接收，如需设置非必需参数，可使用中括号
+    .option('-d, --dev', '获取开发版') // 配置参数，简写和全写中使用,分割
+    .description('创建项目') // 命令描述说明
+    .action(initAction);
+
+commander.parse(process.argv);
